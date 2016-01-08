832af430a451da3e91f51fbdc81061db9965b203
richi <rk@RKs-MacBook-Pro.fritz.box>
2016-01-08 11:09:40 +0100

    Initial Commit
    A (very) small CSS preprocessor. It implements SCSS style variables for
    your css file(s).

---
+++ README.md
@@ -0,0 +1,54 @@
+DynamicCss plugin 0.1.1
+=================
+
+A (very) small CSS preprocessor. It implements SCSS style variables for your css file(s).
+
+Variables are a way to store information that you want to reuse in a stylesheet. Change the value of a variable and all the instances where it is used will update automatically.
+
+Variables have to be declared and initialized:
+
+	$default-margin: 1em;
+	$text-color: #222;
+	$border-color: #bbb;
+
+Later on they can be used:
+
+	blockquote {
+		margin: $default-margin;
+		color: $text-color;
+		border-left: 4px solid $border-color;
+	}
+
+How to install?
+----------------------
+1. Download and install [Yellow](https://github.com/datenstrom/yellow/).  
+2. Download [dynamiccss.php](dynamiccss.php?raw=true), copy it into your `system/plugins` folder. 
+3. Use $variables in the .css file(s) in your `system/themes` folder.  
+
+To uninstall delete the plugin files.
+
+To get it to work with the static site generator (v 0.6.2) you need to change a few lines in the file `commandline.php` in your `system/plugins` folder.
+
+Starting in line 217:
+
+	function buildStaticFile($fileNameSource, $fileNameDest, $fileTypeMedia)
+	{
+		$statusCode = $this->yellow->toolbox->copyFile($fileNameSource, $fileNameDest, true) &&
+				$this->yellow->toolbox->modifyFile($fileNameDest, filemtime($fileNameSource)) ? 200 : 500;
+
+Change to:
+
+	function buildStaticFile($fileNameSource, $fileNameDest, $fileTypeMedia)
+	{
+		$statusCode = $this->pluginCommand(array($fileNameSource, "buildStatic", $fileNameDest, $fileTypeMedia));
+		if ($statusCode == 0)
+		{
+			$statusCode = $this->yellow->toolbox->copyFile($fileNameSource, $fileNameDest, true) &&
+				$this->yellow->toolbox->modifyFile($fileNameDest, filemtime($fileNameSource)) ? 200 : 500;
+		}	
+This code might eat all your data or nuke your server. Use at your own risk.
+
+License
+----------------------
+
+[WTFPL](http://www.wtfpl.net)
---
+++ dynamiccss.php
@@ -0,0 +1,82 @@
+<?php
+// This file may be used and distributed under the terms of the WTF public license.
+
+class YellowDynamicCss
+{
+	const Version = "0.1.1";
+	var $yellow;
+
+	function onLoad($yellow)
+	{
+		$this->yellow = $yellow;
+	}
+	
+	function onRequest($serverScheme, $serverName, $base, $location, $fileName)
+	{
+		$statusCode = 0;
+				
+		if(preg_match("/\.(css)$/", $location) && is_readable($fileName))
+		{
+			$statusCode = 200;
+			
+			$data = $this->parseFile($fileName);		
+			$lastModifiedFormatted = $this->yellow->toolbox->getHttpDateFormatted(filemtime($fileName));
+			@header($this->yellow->toolbox->getHttpStatusFormatted($statusCode));
+			@header("Content-Type: ".$this->yellow->toolbox->getMimeContentType($fileName));
+			@header("Last-Modified: ".$lastModifiedFormatted);
+			echo $data;
+		}
+
+		return $statusCode;
+	}
+
+	function onCommand($args)
+	{
+		list($fileNameSource, $command) = $args;
+		switch($command)
+		{
+			case "buildStatic":	$statusCode = $this->buildStaticCommand($args); break;
+			default: $statusCode = 0;
+		}
+		return $statusCode;
+	}
+	
+	function buildStaticCommand($args)
+	{
+		$statusCode = 0;
+		list($fileNameSource, $command, $fileNameDest, $fileTypeMedia) = $args;
+		
+		if(preg_match("/\.(css)$/", $fileNameSource) && is_readable($fileNameSource))
+		{
+			$statusCode = 200;
+			
+			$data = $this->parseFile($fileNameSource);		
+			$this->yellow->toolbox->createFile($fileNameDest, $data, true);
+
+		}
+		return $statusCode;
+	}
+
+	function parseFile($fileName)
+	{
+		$data = $this->yellow->toolbox->getFileData($fileName);
+
+		$varDefPattern = '/\$([\w\-]+)\s*:(.*?);/';
+		preg_match_all($varDefPattern, $data, $matches);	
+		$varArray = array_combine($matches[1], $matches[2]);
+		array_walk($varArray, function(&$key, &$value) {$key = trim($key); $value = trim($value);});
+	
+		$data = preg_replace($varDefPattern, '', $data);
+		$data = preg_replace("/[\r\n]{3,}/", "\n\n", $data);
+
+		foreach ($varArray as $key => $value)
+		{
+			$data = str_replace('$'.$key, $value, $data);
+		}
+		
+		return $data;
+	}
+}
+
+$yellow->plugins->register("dynamiccss", "YellowDynamicCss", YellowDynamicCss::Version);
+?>
